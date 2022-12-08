fabdb558d38c1fe2e68473dfe2e9bfabca86006d
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2022-12-08 12:53:14 -0500

    chore: rm unused

--- scripts/bridge/is_executable_message.sh
+++
@@ -1,7 +0,0 @@
-#!
-
-source .env
-RECEIVER_ADDRESS=$(cat deploy/addresses.json | jq -j .ReceiverMumbai)
-echo "receiver: $RECEIVER_ADDRESS"
-OUTPUT="$(forge script --rpc-url $RPC_URL_MUMBAI --json IsExectuableMessage -s "run(address)" $RECEIVER_ADDRESS)"
-echo $(echo "$OUTPUT" | grep "{" | jq -j .returns.isExecutable.value)
--- scripts/formatERC20.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# compute the decimal string
-parsed=$(bc -l <<< "$1/10^18")
-
-# convert to hex
-hexed=$(xxd -p <<< x$parsed)
-
-echo $hexed
